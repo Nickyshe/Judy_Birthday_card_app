269d96e77f7fa47f6ad9dc2e8262e1a9f0e51397
efoskhi <efosaosaheni@gmail.com>
2022-10-18 21:55:49 +0100

    Splash screen added

--- app/src/main/java/com/example/judybirthdaycardapp/Splash.java
+++ app/src/main/java/com/example/judybirthdaycardapp/Splash.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
@@ -13,17 +14,12 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        new Handler().postDelayed(new Runnable() {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this,MainActivity.class);
-startActivity(intent);
-finish();
+                setContentView(R.layout.activity_main);
             }
-        },5000);
-
+        }, 5000);
     }
 }
