8097f33987614fdb3036f7871919cb19d3cc90c4
Nickyshen <judypearls200@gmail.com>
2022-10-20 09:10:42 -1100

    New activity and changes in the code

--- app/src/main/java/com/example/judybirthdaycardapp/Sigin_Page.java
+++ app/src/main/java/com/example/judybirthdaycardapp/Sigin_Page.java
@@ -2,7 +2,10 @@ package com.example.judybirthdaycardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Sigin_Page extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Sigin_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sigin_page);
+
+
+    }
+
+
+    public void accountPage(View view) {
+
+        TextView textView = (TextView) findViewById(R.id.account);
+        Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+        startActivity(intent);
     }
 }
